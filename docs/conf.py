d37cd07aa3960a248379d3d43eaa4ef388b32cc2
johnlouieabrina-acn <82374154+johnlouieabrina-acn@users.noreply.github.com>
2021-05-28 10:48:11 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,12 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.append('source')
+
+
+
 
 
 # -- Project information -----------------------------------------------------
@@ -38,7 +42,8 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,7 +60,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
