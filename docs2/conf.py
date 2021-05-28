0b103565bc3c5266007533a48b4de10697c10b31
johnlouieabrina-acn <82374154+johnlouieabrina-acn@users.noreply.github.com>
2021-05-28 11:30:58 +0800

    Update conf.py

--- docs2/conf.py
+++ docs2/conf.py
@@ -10,14 +10,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.append('source')
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PDFExample'
+project = 'No Errors Test Project'
 copyright = '2019, sphinx-action Test Suite'
 author = 'sphinx-action Test Suite'
 
@@ -28,6 +29,16 @@ author = 'sphinx-action Test Suite'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
