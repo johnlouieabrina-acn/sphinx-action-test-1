181e5dbf21db00ab38147836dae23e8fc1b38a47
johnlouieabrina-acn <82374154+johnlouieabrina-acn@users.noreply.github.com>
2021-09-22 11:35:32 +0800

    Update useful.py

--- source/useful.py
+++ source/useful.py
@@ -8,6 +8,19 @@
 
 """
 
+
+def function_with_pep484_type_annotations(param1: int, param2: str) -> bool:
+    """Example function with PEP 484 type annotations.
+
+    Args:
+        param1: The first parameter.
+        param2: The second parameter.
+
+    Returns:
+        The return value. True for success, False otherwise.
+
+    """
+
 def public_fn_with_googley_docstring(name: str, state: bool = None) -> int:
     """TEST This function does something.
 
